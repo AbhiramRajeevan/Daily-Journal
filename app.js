ae4bccf82861fd10c63cf35aaa7a9f67ada311ac
Abhiram Rajeevan <abhiramrajiv14@gmail.com>
2022-01-11 17:33:04 +0530

    Update Port

--- app.js
+++ app.js
@@ -105,6 +105,6 @@ app.get("/delete/:postName",(req,res)=>{
   res.redirect("/")
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });

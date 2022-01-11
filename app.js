76f403daf317099ec0cb9a090a837e02148ef27d
Abhiram Rajeevan <abhiramrajiv14@gmail.com>
2022-01-11 17:28:13 +0530

    Initital Commit

---
+++ app.js
@@ -0,0 +1,110 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var _ = require('lodash');
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+mongoose.connect("mongodb+srv://" + process.env.USER + ":" + process.env.PASS + "@cluster0.uulti.mongodb.net/blogsDB");
+
+const blogSchema ={
+  title : String,
+  content :String
+}
+
+const Blog = mongoose.model("Blog",blogSchema)
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+let posts = []
+
+app.get("/",(req,res)=>{
+  Blog.find({},(err,foundDoc)=>{
+    posts = foundDoc.slice()  //slice -> copies the array content
+  })
+  res.render("home",{
+    startingContent:homeStartingContent,
+    posts:posts});
+});
+
+app.get("/posts/:postName",(req,res)=>{
+  const requestedTitle = _.lowerCase(req.params.postName) //if its Title-1 =>title 1
+
+  Blog.find({},(err,foundDoc)=>{
+    posts = foundDoc.slice()
+  })
+
+  posts.forEach((post)=>{
+    const storedTitle = _.lowerCase(post.title)
+
+    if(storedTitle === requestedTitle){
+      res.render("post",{
+        title : post.title,
+        content : post.content
+      })
+      return
+    }
+  })
+  res.redirect("/")
+})
+app.get("/about",(req,res)=>{
+  res.render("about",{aboutContent:aboutContent});
+});
+
+app.get("/contact",(req,res)=>{
+  res.render("contact",{contactContent:contactContent});
+})
+
+app.get("/compose",(req,res)=>{
+  res.render("compose")
+})
+
+app.post("/compose",(req,res)=>{
+
+  let resp = {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  }
+  const blog = new Blog(resp)
+  //posts.push(post);
+  blog.save()
+  res.redirect("/");
+})
+
+app.get("/delete/:postName",(req,res)=>{
+  const requestedTitle = _.lowerCase(req.params.postName) //if its Title-1 =>title 1
+
+  let id = -1
+
+  posts.forEach(post=>{
+    let title = _.lowerCase(post.title)
+
+    if(title===requestedTitle){
+      id = post._id
+    }
+  })
+
+  if(id!=-1){
+    Blog.findByIdAndRemove(id,(err)=>{
+      if(err)
+        console.log(err);
+    })
+  }
+  res.redirect("/")
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
